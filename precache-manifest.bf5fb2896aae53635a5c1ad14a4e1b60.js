3a929d0b8149c4f6179ec911e72713e734eddc3f
maaheynoor <maaheynoor.s@somaiya.edu>
2020-06-03 15:10:20 +0530

    Updates

---
+++ precache-manifest.bf5fb2896aae53635a5c1ad14a4e1b60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b482f980bcbaca5582c02ea15f83165",
+    "url": "/SmartWatchPreview/index.html"
+  },
+  {
+    "revision": "750a419557508fd811f7",
+    "url": "/SmartWatchPreview/static/css/main.0be3dc10.chunk.css"
+  },
+  {
+    "revision": "f4c190b71e734c74ad86",
+    "url": "/SmartWatchPreview/static/js/2.09aead01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SmartWatchPreview/static/js/2.09aead01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "750a419557508fd811f7",
+    "url": "/SmartWatchPreview/static/js/main.c93ee08f.chunk.js"
+  },
+  {
+    "revision": "072abd2b87bff271153a",
+    "url": "/SmartWatchPreview/static/js/runtime-main.19a23903.js"
+  }
+]);
